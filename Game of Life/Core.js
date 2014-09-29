0badc702664bdff63b1969a3b60b937d7522aa45
zhongyx12 <zhongyx4869@163.com>
2014-09-29 17:28:37 +0800

    细胞自动机

---
+++ Game of Life/Core.js
@@ -0,0 +1,146 @@
+﻿/*
+文件名：Core.js
+版本时间：2014-9-29
+作者：钟仰新
+测试：包煜
+功能：生命游戏的内核代码，用于存储细胞阵列生死信息，以及下一时刻的生死判断。
+*/
+
+//预定义细胞的生死表示规则，1表示细胞生，0表示细胞已死
+var lived = 1;
+var dead = 0;
+
+//以下所有代码为细胞阵列类Grid的定义，包括函数和成员
+
+//构造函数
+function Grid(cfg){
+    for (var attr in cfg){
+        this[attr]=cfg[attr];
+    }
+}
+
+//Grid类原型定义
+Grid.prototype = {
+    constructor: Grid,
+
+    length : 10,    //细胞阵列横向细胞数目，下称“长”
+    width : 10,     //细胞阵列纵向细胞数目，下称“宽”
+
+    cells : null,   //二维数组指针，记录当前细胞阵列的生死情况
+    temp : null,    //二维数组指针，缓存上一时刻细胞阵列的生死情况
+
+    //初始化函数
+    init: function ()
+    {
+        //规定长、宽至少为3
+        this.length = this.length > 3 ? this.length : 3;
+        this.width = this.width > 3 ? this.width : 3;
+
+        //申请cells和temp二维数组，并将细胞信息都初始化为dead
+        this.cells = new Array(this.width);
+        this.temp = new Array(this.width);
+        for (var i = 0; i < this.width; i++)
+        {
+            this.cells[i] = new Array(this.length);
+            this.temp[i] = new Array(this.length);
+            for (var j = 0; j < this.length; j++)
+            {
+                this.cells[i][j] = dead;
+                this.temp[i][j] = dead;
+            }
+        }
+    },
+
+    //改变长、宽的函数，并重新初始化
+    modifySize : function (len, wid)
+    {
+        this.length = len;
+        this.width = wid;
+        this.init();
+    },
+
+    //随机生成函数，执行初始化函数后才能调用，重新随机生成全阵列细胞生死状态
+    random : function ()
+    {
+        var x;
+        for (var i = 0; i < this.width; i++)
+            for (var j = 0; j < this.length; j++)
+            {
+                x = Math.random();
+                x = x > 0.5 ? lived : dead;
+                this.cells[i][j] = x;
+            }
+    },
+
+    //用数组的形式输入细胞阵列的状态，用于测试
+    inputCells_Array : function (arr)
+    {
+        if (arr == null || arr.length == 0 || arr.length != width)
+            return false;
+        for (var i = 0; i < this.width; i++)
+            if (arr[i].length != this.length)
+                return false;
+
+        for (var i = 0; i < this.width; i++)
+            for (var j = 0; j < this.length; j++)
+                this.cells[i][j] = arr[i][j];
+
+        return true;
+    },
+
+    //改变某个位置的细胞生死状态，row横坐标，col纵坐标，state要修改状态
+    inputCells_Pos : function (row, col, state)
+    {
+        if (row > this.width || row < 0 || col > this.length || col < 0)
+            return false;
+        if (state != lived || state != dead)
+            return false;
+
+        this.cells[row][col] = state;
+        return true;
+    },
+    
+    //缓存函数，将当前细胞阵列生死信息cells缓存到缓存数组temp中，辅助jedgeCell函数完成判断
+    duplicate : function ()
+    {
+        for (var i = 0; i < this.width; i++)
+            for (var j = 0; j < this.length; j++)
+                this.temp[i][j] = this.cells[i][j];
+    },
+
+    //判断下一时刻的某位置细胞的生死状况，row为该目标细胞的横坐标，col为纵坐标
+    judgeCell : function (row, col)
+    {
+        //按规则判断，livedNum记录该细胞周围8细胞的活细胞数目，初始化为0
+        var livedNum = 0;
+        var r, c;
+
+        //累计该细胞周围8细胞的活细胞数目
+        for (var i = -1; i < 2; i++)
+            for (var j = -1; j < 2; j++) {
+                if (i == 0 && j == 0)
+                    continue;
+                r = (row + i + this.width) % this.width;
+                c = (col + j + this.length) % this.length;
+                if (this.temp[r][c] == lived)
+                    livedNum++;
+            }
+
+        //按规则判断该细胞的生死，改写cells数组
+        if (livedNum == 3)
+            return lived;
+        if (livedNum == 2)
+            return this.temp[row][col];
+        else
+            return dead;
+    },
+
+    //更新细胞状态的函数，按照规则求出下一时刻细胞阵列的生死状态，并更新cells数组
+    updateCells : function ()
+    {
+        this.duplicate();
+        for (var i = 0; i < this.width; i++)
+            for (var j = 0; j < this.length; j++)
+                this.cells[i][j] = this.judgeCell(i, j);
+    }
+}
---
+++ Game of Life/Show.js
@@ -0,0 +1,260 @@
+﻿/*
+文件名：Shore.js
+版本时间：2014-9-29
+作者：钟仰新
+测试：钟仰新(走读与简单调试)
+功能：生命游戏的展现代码，用于将内核的细胞阵列展现在html页面中。
+*/
+
+//展现层的全局参数
+var mainloop;   //记录展现主循环的id
+var fps = 20;   //展现的刷新帧率
+var sleep = 50; //展现每一帧的刷新时间，fps * sleep == 1000
+var cellSize = 2;   //展现层显示每个细胞的边长，展现形状为正方形
+var grid;   //游戏中的细胞阵列实例
+var canvas; //html的画布控件
+var gc;     //画布空间的“内容”，用于展示层绘制
+var grid_start = false; //记录游戏运行状态，false表示未开始或暂停，true表示进行中
+
+//html输入框控件失去焦点事件函数
+function inputLost(id)
+{
+    var dom = document.getElementById(id);
+    dom.style.backgroundColor = "rgb(175, 175, 175)";
+    dom.style.color = "white";
+}
+
+//html输入框控件获得焦点事件函数
+function inputFocus(id) {
+    var dom = document.getElementById(id);
+    dom.style.backgroundColor = "white";
+    dom.style.color = "black";
+}
+
+//body的onload函数，初始化细胞阵列grid，同时用canvas画出空的细胞阵列背景
+function init()
+{
+    canvas = document.getElementById("MyCanvas");
+    gc = canvas.getContext("2d");
+    grid = new Grid({ length: 200, width: 200});
+
+    grid.init();
+    grid.random();  //随机生成细胞信息
+
+    //画出空的细胞阵列背景
+    gc.fillStyle = "orange";
+    gc.fillRect(0, 0, grid.length, grid.width);
+}
+
+//主循环的执行函数，在画布上画出当前细胞阵列状态，同时判断下一时刻状态
+function run()
+{
+    //绘制细胞阵列
+    for (var i = 0; i < grid.width; i++)
+        for (var j = 0; j < grid.length; j++)
+        {
+            if (grid.cells[i][j] == grid.temp[i][j])
+                continue;
+            if (grid.cells[i][j] == lived)
+                gc.fillStyle = "green";
+            else
+                gc.fillStyle = "orange";
+            gc.fillRect(j, i, 1, 1);
+        }
+
+    //更新下一时刻细胞阵列信息
+    grid.updateCells();
+}
+
+//html开始/暂停按钮的onclick函数，用于开始/暂停游戏主循环
+function start_pause()
+{
+    var btn;
+    if (grid_start == false)    //游戏本来处于暂停态，准备开始
+    {
+        mainloop = setInterval(run, sleep); //开启主循环
+        btn = document.getElementById("start_pause");
+        btn.setAttribute("value", "暂停");
+        grid_start = true;
+    }
+    else    //游戏本来处于进行态，准备暂停
+    {
+        window.clearInterval(mainloop); //暂停主循环
+        btn = document.getElementById("start_pause");
+        btn.setAttribute("value", "开始");
+        grid_start = false;
+    }
+}
+
+//html随机按钮的onclick函数，重新随机生成细胞阵列
+function random()
+{
+    if (grid_start == true) //如果主循环进行中，先暂停再重新初始化
+        window.clearInterval(mainloop);
+    var btn = document.getElementById("start_pause");
+    btn.setAttribute("value", "开始");
+    grid_start = false;
+    grid.random();  //重新初始化细胞阵列
+    run();
+}
+
+//html中修改FPS按钮的onclick函数，修改游戏展示的FPS帧数
+function modify_fps()
+{
+    var text = document.getElementById("fps");
+    var newfps = parseInt(text.value);
+
+    //异常输入处理
+    if (text.value == "")
+        return false;
+    if (window.isNaN(newfps))
+    {
+        alert("FPS请输入一个整数");
+        return false;
+    }
+    if (newfps <= 0 || newfps > 1000)
+    {
+        alert("FPS的范围是(0,1000]");
+        return false;
+    }
+
+    text.value = "";
+    fps = newfps;
+    sleep = 1000.0 / newfps;
+
+    var showText = document.getElementById("showFPS");
+    showText.innerText = newfps + "";
+
+    if (grid_start == true) //如果游戏主循环进行中，则停下主循环，使用新的FPS(sleep间隔时间)进行新的主循环
+    {
+        window.clearInterval(mainloop);
+        mainloop = setInterval(run, sleep);
+    }
+    return true;
+}
+
+//html修改细胞大小按钮的onclick函数，改变细胞显示大小，相当于放大缩小按键
+function modify_cellSize()
+{
+    var text = document.getElementById("size");
+    var newcs = parseInt(text.value);
+
+    //异常输入处理
+    if (text.value == "")
+        return false;
+    if (window.isNaN(newcs)) {
+        alert("细胞大小请输入一个整数");
+        return false;
+    }
+    if (newcs <= 0 || newcs > 10) {
+        alert("细胞大小的范围是[1,10]");
+        return false;
+    }
+
+    text.value = "";
+    cellSize = newcs;
+
+    var showText = document.getElementById("showSize");
+    showText.innerText = newcs + "";
+
+    //修改canvas实际大小，达到缩放细胞的效果
+    canvas.style.width = grid.length * cellSize + "px";
+    canvas.style.height = grid.width * cellSize + "px";
+    return true;
+}
+
+//html修改细胞大小的"+""-"按钮的onclick函数，改变细胞显示大小，相当于放大缩小按键
+function justify_cellSize(offset) {
+    var newcs = cellSize + offset;
+
+    //限制细胞大小只能为1~10的整数
+    newcs = newcs > 10 ? 10 : newcs;
+    newcs = newcs < 1 ? 1 : newcs;
+
+    cellSize = newcs;
+    var showText = document.getElementById("showSize");
+    showText.innerText = newcs + "";
+
+    //修改canvas实际大小，达到缩放细胞的效果
+    canvas.style.width = grid.length * cellSize + "px";
+    canvas.style.height = grid.width * cellSize + "px";
+    return true;
+}
+
+//html修改阵列尺寸按钮的onclick函数，改变细胞阵列横向、纵向细胞数目
+function modify_arraySize() {
+    var textLen = document.getElementById("length");
+    var newLen = parseInt(textLen.value);
+    var textWid = document.getElementById("width");
+    var newWid = parseInt(textWid.value);
+    var lenflag = 1, widflag = 1;   //标志，记录玩家有没输入新的长和宽，有输入为1，没输入为0
+
+    if (textLen.value == "" && textWid.value == "")
+        return false;
+    if (textWid.value == "")
+        widflag = 0;
+    if (textLen.value == "")
+        lenflag = 0;
+
+    //异常输入处理
+    if (lenflag == 1 && window.isNaN(newLen)) {
+        alert("横向细胞数目请输入一个整数");
+        return false;
+    }
+    if (lenflag == 1 && (newLen < 3 || newLen > 1000)) {
+        alert("横向细胞数目的范围是[3,1000]");
+        return false;
+    }
+
+    if (widflag == 1 && window.isNaN(newWid)) {
+        alert("纵向细胞数目请输入一个整数");
+        return false;
+    }
+    if (widflag == 1 && (newWid < 3 || newWid > 1000)) {
+        alert("纵向细胞数目的范围是[3,1000]");
+        return false;
+    }
+    
+    textLen.value = "";
+    textWid.value = "";
+
+    //如果正在进行主循环，则停下，便于修改长宽的操作
+    if (grid_start == true) {
+        window.clearInterval(mainloop);
+        var btn = document.getElementById("start_pause");
+        btn.setAttribute("value", "开始");
+        grid_start = false;
+    }
+
+    //清空细胞阵列，即清空canvas
+    gc.clearRect(0, 0, grid.length, grid.width);
+
+    //修改长、宽（如果用户有输入）
+    if (lenflag == 1)
+    {
+        grid.length = newLen;
+        var showText = document.getElementById("showLen");
+        showText.innerText = newLen + "";
+    }   
+    if (widflag == 1)
+    {
+        grid.width = newWid;
+        var showText = document.getElementById("showWid");
+        showText.innerText = newWid + "";
+    }
+
+    //重新根据新长、宽来初始化随机生成细胞阵列
+    grid.init();
+    grid.random();
+
+    //重新设置canvas的逻辑长宽和实际长宽
+    canvas.setAttribute("width", grid.length + "");
+    canvas.setAttribute("height", grid.width + "");
+    canvas.style.width = grid.length * cellSize + "px";
+    canvas.style.height = grid.width * cellSize + "px";
+
+    //重新绘制空白的细胞阵列背景
+    gc.fillStyle = "orange";
+    gc.fillRect(0, 0, grid.length, grid.width);
+    return true;
+}
